a4ab07bc2433859c270eb6c65eca86181943cfaa
unknown <matiasvaldez8184@gmail.com>
2025-09-03 01:55:24 -0300

    deployment error fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.googleusercontent.com',
+      }
+    ],
+  },
 }
 
 module.exports = {
-  nextConfig,
+  ...nextConfig,
   webpack: (config, options) => {
     config.module.generator.asset.publicPath = "/_next/";
     config.module.rules.push({
@@ -16,6 +36,6 @@ module.exports = {
     })
 
     return config
-},
+  },
 }
 
