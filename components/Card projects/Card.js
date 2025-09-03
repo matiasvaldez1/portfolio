214e7e382fd9393f7a6352306126a24280b24981
unknown <matiasvaldez8184@gmail.com>
2025-09-03 01:52:59 -0300

    deployment error fix

--- components/Card projects/Card.js
+++ components/Card projects/Card.js
@@ -12,8 +12,11 @@ export const Card = ({image,title,github,deploy}) => {
     whileInView={{ opacity: 1 }}
     transition={{duration: 1}}
     className={styles.container}>
-      <img 
+      <Image 
       src={image}
+      alt={title || "Project image"}
+      width={320}
+      height={180}
       className={styles.img}
       />
       <div className={styles.text}>
